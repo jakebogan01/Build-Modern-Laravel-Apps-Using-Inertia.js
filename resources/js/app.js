d92a7127a2418866e3934a1125b183d458e4ef94
Jake Bogan <jakebogan01@gmail.com>
2023-08-11 12:46:58 -0400

    default layouts

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,20 @@ import { createApp, h } from 'vue';
 import {createInertiaApp, Link} from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+// import Layout from "@/Shared/Layout.vue";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    // if you want to use a custom layout, uncomment this line and remove the layout import from all pages
+    // resolve: name => {
+    //     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    //     let page = pages[`./Pages/${name}.vue`]
+    //     page.default.layout = page.default.layout || Layout
+    //     return page
+    // },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
